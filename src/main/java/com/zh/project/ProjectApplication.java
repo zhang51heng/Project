ff9b9696fbd8a12b61336337482b0eb174ebaf23
zhangheng <zhangheng>
2019-06-12 17:39:33 +0800

    项目初始化

---
+++ src/main/java/com/zh/project/ProjectApplication.java
@@ -0,0 +1,15 @@
+package com.zh.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+
+}
